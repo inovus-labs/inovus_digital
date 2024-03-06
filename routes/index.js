2ecd4fe24abe0f28de8cce1564f9fc62430c1a96
Milan Sony <milansonyofficial@gmail.com>
2024-03-07 00:40:30 +0530

    user data added to db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const userHelpers = require('../helpers/user_helpers')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
@@ -10,17 +12,45 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/users', function(req, res, next) {
-  
+router.get('/users', function (req, res, next) {
+
   res.render('userform', {
     title: 'Inovus Digital',
     style: 'userformpage.css',
   });
 });
 
-router.get('/live', (req, res)=>{
+router.post('/add_user_info', (req, res) => {
 
-  res.render('live_users',{
+  console.log(req.body)
+  userHelpers.addUserInfo(req.body).then((response) => {
+
+    console.log(response)
+
+    let userID = response.insertedId
+    let acknowledge = response.acknowledged
+
+    console.log(userID, acknowledge)
+
+    let image = req.files.image
+
+    image.mv('./public/user_images/' + userID + '.jpg', (err, done) => {
+      if (!err) {
+        res.render('userform', {
+          alert: acknowledge,
+          title: 'Inovus Digital',
+          style: 'userformpage.css',
+        })
+      } else {
+        console.log("Error" + err)
+      }
+    })
+  })
+})
+
+router.get('/live', (req, res) => {
+
+  res.render('live_users', {
     title: 'live users',
     header: true,
     style: 'live_users.css'
