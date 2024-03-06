a1b6079dfe0c3bfb0e1e21f1d0ceae9ed0a4f7f8
Milan Sony <milansonyofficial@gmail.com>
2024-03-07 00:38:19 +0530

    connection and collection created

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const url = "mongodb://127.0.0.1:27017/inovuslive";
+
+module.exports = {
+
+    connect: () => {
+        mongoose.connect(url)
+            .then(() => {
+                console.log("Connected to MongoDB\n");
+            }).catch((error) => {
+                console.log("MongoDB not connected\n" + error);
+            });
+    },
+
+    collection: (name) => {
+        return mongoose.connection.db.collection(name);
+    }
+};
