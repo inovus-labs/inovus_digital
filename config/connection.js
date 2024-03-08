84b6eb74f586333c3cf3776e19039d70466da8c1
Milan Sony <milansonyofficial@gmail.com>
2024-03-09 01:20:14 +0530

    MongoDB connection string changed

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const url = "mongodb://127.0.0.1:27017/inovuslive";
+// const url = "mongodb://127.0.0.1:27017/inovuslive";
+const url = "mongodb+srv://milan-sony:hpWvfwIgWUQmIB3A@inovus-labs.lwks89q.mongodb.net/inovus_digital";
 
 module.exports = {
 
