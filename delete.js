44bcd8f8eddcbfd63578c4027fe6476d4f1f0d07
arjun.airtory <arjun12345krishna@gmail.com>
2024-03-11 01:39:57 +0530

    Cron Delete

---
+++ delete.js
@@ -0,0 +1,20 @@
+
+require("dotenv").config()
+const mongoose = require('mongoose');
+const userHelpers = require('./helpers/user_helpers.js')
+
+
+mongoose.connect(process.env.mongoDB_URL)
+    .then(() => {
+
+        console.log("\n--- Connected to MongoDB Atlas");
+
+        userHelpers.dropCollection().then(() => {
+            console.log("--- Data deleted successfully\n");
+        }).catch((error) => {
+            console.log("--- Error deleting all users\n", error);
+        });
+
+    }).catch((error) => {
+        console.log("\n --- Error connecting to MongoDB Atlas\n", error);
+    });
