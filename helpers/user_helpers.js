42a29abc619fee7885e82067bf89a685e72c8acd
Milan Sony <milansonyofficial@gmail.com>
2024-03-12 11:54:48 +0530

    [feat] - users displayed on /live page based on role

--- helpers/user_helpers.js
+++ helpers/user_helpers.js
@@ -12,17 +12,53 @@ module.exports = {
         })
     },
 
-    getAllUsers: () =>{
-        return new Promise((resolve, reject)=>{
-            db.collection(collections.LIVE_COLLECTIONS).find().toArray().then((userData)=>{
+    getAllUsers: () => {
+        return new Promise((resolve, reject) => {
+            db.collection(collections.LIVE_COLLECTIONS).find().toArray().then((userData) => {
                 resolve(userData)
             })
         })
     },
 
-    dropCollection: () =>{
-        return new Promise((resolve, reject)=>{
-            db.collection(collections.LIVE_COLLECTIONS).drop().then((response)=>{
+    getAllMentors: () => {
+        return new Promise(async (resolve, reject) => {
+            await db.collection(collections.LIVE_COLLECTIONS).find({ role: "Mentor" }).toArray().then((mentors) => {
+                console.log("Mentors are:", mentors)
+                resolve(mentors)
+            }).catch((err) => {
+                console.log("mentors collection error", err)
+                reject(err)
+            })
+        })
+    },
+
+    getAllLearners: ()=>{
+        return new Promise(async(resolve, reject) => {
+            await db.collection(collections.LIVE_COLLECTIONS).find({$or: [{ role: "Learning" },{ role: "Building a Project" }]}).toArray().then((learners)=>{
+                console.log("Learners are:", learners)
+                resolve(learners)
+            }).catch((err)=>{
+                console.log("Listener collection error", err)
+                reject(err)
+            })
+        })
+    },
+
+    getAllExplores: () => {
+        return new Promise(async (resolve, reject) => {
+            await db.collection(collections.LIVE_COLLECTIONS).find({ role: "Explore" }).toArray().then((explores) => {
+                console.log("Explorers are:", explores)
+                resolve(explores)
+            }).catch((err) => {
+                console.log("explores collection error", err);
+                reject(err)
+            })
+        })
+    },
+
+    dropCollection: () => {
+        return new Promise((resolve, reject) => {
+            db.collection(collections.LIVE_COLLECTIONS).drop().then((response) => {
                 resolve(response)
             })
         })
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const userHelpers = require('../helpers/user_helpers')
+const userHelpers = require('../helpers/user_helpers');
 
 
 // GET home page
@@ -31,23 +31,66 @@ router.post('/users', (req, res) => {
 
 
 // GET live users page
-router.get('/live', (req, res) => {
-  userHelpers.getAllUsers().then((userData) => {
-    if (userData.length >= 6) {
-      res.render('live_users', {
-        title: 'Live',
-        header: true,
-        style: 'live_users.css',
-        allUsersData: userData
-      })
-    } else {
-      res.render('live_users', {
-        title: 'Live',
-        header: true,
-        style: 'live_users.css',
-        users: userData
-      })
-    }
+// router.get('/live', (req, res) => {
+//   userHelpers.getAllUsers().then((userData) => {
+//     if (userData.length >= 6) {
+//       res.render('live_users', {
+//         title: 'Live',
+//         header: true,
+//         style: 'live_users.css',
+//         allUsersData: userData
+//       })
+//     } else {
+//       res.render('live_users', {
+//         title: 'Live',
+//         header: true,
+//         style: 'live_users.css',
+//         users: userData
+//       })
+//     }
+//   })
+// })
+
+
+// GET all users in the category mentor
+router.get('/live', async (req, res) => {
+
+  let mentors = {}
+  let learners = {}
+  let explores = {}
+
+  await userHelpers.getAllMentors().then((mentorsData) => {
+    console.log("Mentors Data", mentorsData)
+    mentors = mentorsData
+  }).catch((err) => {
+    console.log(err)
+  })
+
+  await userHelpers.getAllLearners().then((learnersData) => {
+    console.log("Leaners Data", learnersData)
+    learners = learnersData
+  }).catch((err) => {
+    console.log(err)
+  })
+
+  await userHelpers.getAllExplores().then((exploresData) => {
+    console.log("Explorers Data", exploresData)
+    explores = exploresData
+  }).catch((err) => {
+    console.log(err);
+  })
+
+  console.log("mentors : ", mentors)
+  console.log("Learners : ", learners)
+  console.log("Explorers : ", explores)
+
+  res.render('live_users', {
+    title: 'Live',
+    header: true,
+    style: 'live_users.css',
+    mentors,
+    learners,
+    explores
   })
 })
 
